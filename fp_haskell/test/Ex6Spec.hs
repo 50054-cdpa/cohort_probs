8cf2dc8865f1dce70c8aead0d4641e64e045e042
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2024-09-07 16:28:35 +0800

    updated the indentation

--- fp_haskell/test/Ex6Spec.hs
+++ fp_haskell/test/Ex6Spec.hs
@@ -6,9 +6,9 @@ import Ex6 (rotate)
 
 spec :: Spec
 spec = do
-  describe "Ex6Spec" $ do 
-    it "test ex6" $
-        let input = [[1,2,3], [4,5,6], [7,8,9]]
-            result = rotate input  
-            expected = [[7,4,1], [8,5,2], [9,6,3]] 
-        in result `shouldBe` expected
+    describe "Ex6Spec" $ do 
+        it "test ex6" $
+            let input = [[1,2,3], [4,5,6], [7,8,9]]
+                result = rotate input  
+                expected = [[7,4,1], [8,5,2], [9,6,3]] 
+            in result `shouldBe` expected
